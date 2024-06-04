72ee456fc06ab0c3add681ec712a18a4e8483b56
Sanket Dadali <sanketdadali05@gmail.com>
2024-06-04 18:00:25 +0530

    Hono with cloudfare workers

---
+++ Hono/my-app/src/index.ts
@@ -0,0 +1,32 @@
+import { Hono, Next } from "hono";
+import { Context } from "hono/jsx";
+
+const app = new Hono();
+
+//middleware in Hono
+async function authMiddleware(c: any, next: any) {
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next();
+  } else {
+    return c.text("You dont have acces");
+  }
+}
+
+app.use(authMiddleware);
+
+app.get("/redirect", async (c) => {
+  const body = await c.req.parseBody();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.json({ msg: "as" });
+});
+
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
